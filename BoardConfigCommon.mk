e089ef463687296a01f390d58e5f7e8ee6f7101a
zlewchan <wiktorusyt@protonmail.ch>
2022-07-03 18:00:01 +0000

    msm8937-common: Set TARGET_KERNEL_USE_DEFAULT_CLANG to true
    Signed-off-by: zlewchan <wiktorusyt@protonmail.ch>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,6 +109,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
+TARGET_KERNEL_USE_DEFAULT_CLANG := true
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
